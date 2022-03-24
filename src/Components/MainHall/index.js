e7acde33fcaec308f2fed3e6b3ff4c622df4876d
deepali-02 <deepali93.redekar@gmail.com>
2022-03-24 10:59:57 +0100

    added indication of different ranks and screen for theater

--- src/Components/MainHall/index.js
+++ src/Components/MainHall/index.js
@@ -4,6 +4,23 @@ import { groups } from "../../Data/group";
 export default function MainHall() {
   console.log("Layout", layout);
   return <>
+  <ul class="showcase">
+        <li>
+          <div class="rank1"></div>
+          <small>Rank 1</small>
+        </li>
+        <li>
+          <div class="rank2"></div>
+          <small>Rank 2</small>
+        </li>
+        <li>
+          <div class="rank3"></div>
+          <small>Rank 3</small>
+        </li>
+      </ul>
+      <div className="scontainer">
+        <div className="screen mt-5"></div>
+      </div>
   <div className="scontainer">
         {layout.sections.map((section) => {
           if (section.name === "main hall") {
