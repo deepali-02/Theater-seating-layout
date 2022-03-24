f2d797415f229b6e9e071a36b48592409b274d8b
deepali-02 <deepali93.redekar@gmail.com>
2022-03-24 10:53:39 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import MainHall from './Components/MainHall'
 
 function App() {
   return (
     <div className="App">
-     
+    <Routes>
+      <Route path ="/" element={<Home/>}/>
+      <Route path="/mainHall" element={<MainHall/>}/>
+    </Routes>
       
     </div>
   );
