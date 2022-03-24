2ece48ddf7d9901e3c78d8aea26d8d9bd27b1cb0
deepali-02 <deepali93.redekar@gmail.com>
2022-03-24 10:56:06 +0100

    display seating layout for main hall

--- src/Components/MainHall/index.js
+++ src/Components/MainHall/index.js
@@ -2,5 +2,29 @@ import { layout } from "../../Data/layout";
 import { groups } from "../../Data/group";
 
 export default function MainHall() {
-  return <></>;
+  return <>
+  <div className="scontainer">
+        {layout.sections.map((section) => {
+          if (section.name === "main hall") {
+            return section.rows.map((row) => {
+            
+
+              return (
+                <>
+                  <div className="row">
+                    {row.seats.map((seat) => {
+                      return (
+                        <div className={seat.rank}>
+                          <div className="seat">{seat.seat}</div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </>
+              );
+            });
+          }
+        })}
+      </div>
+  </>;
 }
