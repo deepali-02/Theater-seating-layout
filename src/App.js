cca90a056c6f923785dc2c7c417fe72b72705033
deepali-02 <deepali93.redekar@gmail.com>
2022-03-25 16:31:56 +0100

    added route for seatingLayout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import MainHall from './Components/MainHall'
+import SeatingLayout from './Components/SeatingLayout';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path ="/" element={<Home/>}/>
       <Route path="/mainHall" element={<MainHall/>}/>
+      <Route path="/seatingLayout" element={<SeatingLayout/>}/>
     </Routes>
       
     </div>
