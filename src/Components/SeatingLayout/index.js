fa8fa545c535881f46eefcc8114fd6fa72c238b8
deepali-02 <deepali93.redekar@gmail.com>
2022-03-25 10:40:35 +0100

    modify layout data structure

--- src/Components/SeatingLayout/index.js
+++ src/Components/SeatingLayout/index.js
@@ -1,7 +1,17 @@
 import { layout } from "../../Data/layout";
 import { groups } from "../../Data/group";
 
-export default function SeatingLayout (){
+export default function SeatingLayout() {
+  const newLayoutSections = layout.sections.reduce((acc, section) => {
+    return {
+      ...acc,
+      [section.name]: section.rows.map((r) => r.seats),
+    };
+  }, {});
 
-    
+  const newLayout={ranks:layout.ranks, sections:newLayoutSections}
+
+  console.log("New Layout",newLayout)
+
+  return <div></div>;
 }
