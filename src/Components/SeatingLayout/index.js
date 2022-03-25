0a3bc811fab09f306594c33c971ddf5749cc4326
deepali-02 <deepali93.redekar@gmail.com>
2022-03-25 11:22:49 +0100

    showed seating layout with assigned groups

--- src/Components/SeatingLayout/index.js
+++ src/Components/SeatingLayout/index.js
@@ -5,13 +5,28 @@ export default function SeatingLayout() {
   const newLayoutSections = layout.sections.reduce((acc, section) => {
     return {
       ...acc,
-      [section.name]: section.rows.map((r) => r.seats),
+      [section.name]: section.rows.map((r) => r.seats)
     };
   }, {});
 
-  const newLayout={ranks:layout.ranks, sections:newLayoutSections}
+  const newLayout = { ranks: layout.ranks, sections: newLayoutSections };
 
-  console.log("New Layout",newLayout)
+  console.log("New Layout", newLayout);
+
+  const layoutWithSeating = { ...newLayout };
+
+  groups.forEach((group, i) => {
+    group.seats.forEach((seat) => {
+      layoutWithSeating.sections[seat.section][parseInt(seat.row)-1][
+        parseInt(seat.seat)
+      ] = {
+        ...newLayout.sections[seat.section][parseInt(seat.row)-1][parseInt(seat.seat)-1],
+        group: `g${i + 1}`,
+      };
+    });
+  });
+
+  console.log("Layout with seating arrengment", layoutWithSeating)
 
   return <div></div>;
 }
