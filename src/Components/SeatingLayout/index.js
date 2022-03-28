92057a17ef7e4669b171eb3cfec8e879d6675382
deepali-02 <deepali93.redekar@gmail.com>
2022-03-28 16:33:43 +0200

    map over layoutWithSeating object to console log seat with group

--- src/Components/SeatingLayout/index.js
+++ src/Components/SeatingLayout/index.js
@@ -10,26 +10,13 @@ export default function SeatingLayout() {
     };
   }, {});
 
-  //   console.log("new sections", newLayoutSections);
+  console.log("new sections", newLayoutSections);
 
   const newLayout = { ranks: layout.ranks, sections: newLayoutSections };
 
   console.log("New Layout", newLayout);
 
   const layoutWithSeating = { ...newLayout };
-
-  //   groups.forEach((group, i) => {
-  //     group.seats.forEach((seat) => {
-  //       layoutWithSeating.sections[seat.section][parseInt(seat.row)-1][
-  //         parseInt(seat.seat)-1
-  //       ] = {
-  //         ...newLayout.sections[seat.section][parseInt(seat.row)-1][parseInt(seat.seat)-1],
-  //         group: `g${i + 1}`,
-  //       };
-  //     });
-  //   });
-
-  //   console.log("Layout with seating arrengment", layoutWithSeating)
 
   groups.map((group, i) => {
     return group.seats.map((seat) => {
@@ -43,48 +30,21 @@ export default function SeatingLayout() {
       };
     });
   });
-  console.log(
-    "Layout with seating arrengment",
-    layoutWithSeating
-  );
+  console.log("Layout with seating arrengment", layoutWithSeating);
 
   // const index= groups.findIndex(obj=>{return obj.id})
 
   // console.log("index",index)
 
-  // layoutWithSeating.sections.map((row, i) => {
-  //   return row[i].map((seat) => {
-  //     return seat.seat / seat.group;
-  //   });
-  // });
-
-    // Object.keys(layoutWithSeating.sections).map((section)=>{
-    //   return(
-    //     layoutWithSeating.sections["main hall"].map((row,i)=>{
-    //         return row[i].map((seat)=>{
-    //           return console.log(`seat: ${seat.seat}/${seat.group}`)
-    //         })
-    //     })
-    //   )
-    // })
-
-    Object.keys(layoutWithSeating.sections).map((section)=>{
-      return(
-        layoutWithSeating.sections["main hall"].map((row,i)=>{
-          return(
-          Object.keys(row).map((seat)=>{
-           return(
-             
-                console.log(`${row[seat].seat}/${row[seat].group}`)
-                
-        
-           )
-
-          })
-          )
-        })
-      )
-    })
+  Object.keys(layoutWithSeating.sections).map((section) => {
+    return layoutWithSeating.sections["main hall"].map((row) => {
+      return Object.keys(row).map((seat) => {
+        return row[seat].group === undefined
+          ? console.log(row[seat].seat)
+          : console.log(`${row[seat].seat}/[${row[seat].group}]`);
+      });
+    });
+  });
 
   return (
     <div>
