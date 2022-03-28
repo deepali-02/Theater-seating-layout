91f4daab5fc9b7fd6a515b6493ada634f993fbe3
deepali-02 <deepali93.redekar@gmail.com>
2022-03-28 21:53:20 +0200

    added class for button

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
+    <>
     <div className="container">
       <h1>Welcome to Theater Seating Layout</h1>
-
+</div>
+<div>
       <Link to="/theater">
-        <button>Show Seats!</button>
+        <button class="button button1">Show Seats!</button>
       </Link>
     </div>
+    </>
   );
 }
