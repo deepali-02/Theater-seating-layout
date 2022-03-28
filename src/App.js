38ea13de51c94e88f54bc0daa3469eea41999004
deepali-02 <deepali93.redekar@gmail.com>
2022-03-28 20:32:12 +0200

    inster route for Theater page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import MainHall from './Components/MainHall'
 import SeatingLayout from './Components/SeatingLayout';
+import Theater from './Pages/Theater';
 
 function App() {
   return (
     <div className="App">
     <Routes>
       <Route path ="/" element={<Home/>}/>
-      <Route path="/mainHall" element={<MainHall/>}/>
+      <Route path="/theater" element={<Theater/>}/>
       <Route path="/seatingLayout" element={<SeatingLayout/>}/>
+      <Route path="/mainHall" element={<MainHall/>}/>
     </Routes>
       
     </div>
