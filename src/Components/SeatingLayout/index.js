2dd237c0d24843437f2f0f9b8937c312b32f0f92
deepali-02 <deepali93.redekar@gmail.com>
2022-03-28 16:39:15 +0200

    update group data structure to add group in seats

--- src/Components/SeatingLayout/index.js
+++ src/Components/SeatingLayout/index.js
@@ -3,52 +3,76 @@ import { groups } from "../../Data/group";
 import MainHall from "../MainHall";
 
 export default function SeatingLayout() {
-  const newLayoutSections = layout.sections.reduce((acc, section) => {
-    return {
-      ...acc,
-      [section.name]: section.rows.map((r) => r.seats),
-    };
-  }, {});
 
-  console.log("new sections", newLayoutSections);
+  const groupSeats = [];
 
-  const newLayout = { ranks: layout.ranks, sections: newLayoutSections };
-
-  console.log("New Layout", newLayout);
-
-  const layoutWithSeating = { ...newLayout };
-
-  groups.map((group, i) => {
-    return group.seats.map((seat) => {
-      layoutWithSeating.sections[seat.section][parseInt(seat.row) - 1][
-        parseInt(seat.seat) - 1
-      ] = {
-        ...newLayout.sections[seat.section][parseInt(seat.row) - 1][
-          parseInt(seat.seat) - 1
-        ],
-        group: `g${i + 1}`,
-      };
+  groups.forEach((group, groupIndex) => {
+    group.seats.forEach((seat) => {
+      groupSeats.push({ ...seat, group: groupIndex + 1 });
     });
   });
-  console.log("Layout with seating arrengment", layoutWithSeating);
 
-  // const index= groups.findIndex(obj=>{return obj.id})
+  console.log("groups seats", groupSeats);
 
-  // console.log("index",index)
 
-  Object.keys(layoutWithSeating.sections).map((section) => {
-    return layoutWithSeating.sections["main hall"].map((row) => {
-      return Object.keys(row).map((seat) => {
-        return row[seat].group === undefined
-          ? console.log(row[seat].seat)
-          : console.log(`${row[seat].seat}/[${row[seat].group}]`);
-      });
-    });
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+  // const newLayoutSections = layout.sections.reduce((acc, section) => {
+  //   return {
+  //     ...acc,
+  //     [section.name]: section.rows.map((r) => r.seats),
+  //   };
+  // }, {});
+
+  // console.log("new sections", newLayoutSections);
+
+  // const newLayout = { ranks: layout.ranks, sections: newLayoutSections };
+
+  // console.log("New Layout", newLayout);
+
+  // const layoutWithSeating = { ...newLayout };
+
+  // groups.map((group, i) => {
+  //   return group.seats.map((seat) => {
+  //     layoutWithSeating.sections[seat.section][parseInt(seat.row) - 1][
+  //       parseInt(seat.seat) - 1
+  //     ] = {
+  //       ...newLayout.sections[seat.section][parseInt(seat.row) - 1][
+  //         parseInt(seat.seat) - 1
+  //       ],
+  //       group: `g${i + 1}`,
+  //     };
+  //   });
+  // });
+  // console.log("Layout with seating arrengment", layoutWithSeating);
+
+  // // const index= groups.findIndex(obj=>{return obj.id})
+
+  // // console.log("index",index)
+
+  // Object.keys(layoutWithSeating.sections).map((section) => {
+  //   return layoutWithSeating.sections["main hall"].map((row) => {
+  //     return Object.keys(row).map((seat) => {
+  //       return row[seat].group === undefined
+  //         ? console.log(row[seat].seat)
+  //         : console.log(`${row[seat].seat}/[${row[seat].group}]`);
+  //     });
+  //   });
+  // });
 
   return (
     <div>
-      <MainHall seatingLayout={layoutWithSeating} />
+      <MainHall />
     </div>
   );
 }
