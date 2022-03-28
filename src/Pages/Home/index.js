9241eb4ef39297652bc5c6626b7b427cf5443269
deepali-02 <deepali93.redekar@gmail.com>
2022-03-28 16:35:54 +0200

    remove input field and bootstrap

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,43 +1,13 @@
-import { Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <Container className="mt-5">
+    <div className="container">
       <h1>Welcome to Theater Seating Layout</h1>
-      <h3>Want to reserve seats for you?? </h3>
-      <div className="sectionContainer">
-        <label> Select a section:</label>
-        <select>
-          <option value="mainHall">Main Hall</option>
-          <option value="1stBalcony">!st Balcony</option>
-          <option value="loge">Loge</option>
-        </select>
-        <div>
-          <label>Phone No. </label>
-          <input
-            type="tel"
-            id="myform_phone"
-            name="phone"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            required
-            placeholder="Enter Phone Number"
-
-          />
-        </div>
-        <div>
-          <label>Number of seats:</label>
-          <input
-            type="number"
-            placeholder="How many seats you want to reserve?"
-            min="1"
-          />
-        </div>
-      </div>
 
       <Link to="/seatingLayout">
-        <Button>Reserve Seats!</Button>
+        <button>Show Seats!</button>
       </Link>
-    </Container>
+    </div>
   );
 }
