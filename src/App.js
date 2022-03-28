ff26fde29cd9134447cc3dd072b9f7b2f7568298
deepali-02 <deepali93.redekar@gmail.com>
2022-03-28 21:51:06 +0200

    added route for FirstBalcony

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
-import MainHall from './Components/MainHall'
-import SeatingLayout from './Components/SeatingLayout';
-import Theater from './Pages/Theater';
+import Home from "./Pages/Home";
+import MainHall from "./Components/MainHall";
+import SeatingLayout from "./Components/SeatingLayout";
+import Theater from "./Pages/Theater";
+import FirstBalcony from "./Components/FirstBalcony";
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <Route path ="/" element={<Home/>}/>
-      <Route path="/theater" element={<Theater/>}/>
-      <Route path="/seatingLayout" element={<SeatingLayout/>}/>
-      <Route path="/mainHall" element={<MainHall/>}/>
-    </Routes>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/theater" element={<Theater />} />
+        <Route path="/seatingLayout" element={<SeatingLayout />} />
+        <Route path="/mainHall" element={<MainHall />} />
+        <Route path="/firstBalcony" element={<FirstBalcony />} />
+      </Routes>
     </div>
   );
 }
